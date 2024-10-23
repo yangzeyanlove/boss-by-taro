b19a8e004b96da0f2ed5118d77c542ca39d6daaa
yangzy01 <yangzy01@mingyuanyun.com>
2024-10-23 12:18:29 +0800

    feat: 底部导航栏

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,11 +1,53 @@
+// 主题配置
+export const themeConfig = {
+  primaryColor: '#17B3B3',
+  color: '#B8B8B8',
+};
+
+// 应用相关配置
 export default defineAppConfig({
   pages: [
-    'pages/index/index'
+    'pages/index/index',
+    'pages/position/index',
+    'pages/chat/index',
+    'pages/my/index',
   ],
+  tabBar: {
+    list: [
+      {
+        pagePath: 'pages/index/index',
+        text: '找工作',
+        iconPath: 'assets/images/work.png', // 替换为你的图标类名
+        selectedIconPath: 'assets/images/work-on.png',
+      },
+      {
+        pagePath: 'pages/position/index',
+        text: '深圳',
+        iconPath: 'assets/images/position.png',
+        selectedIconPath: 'assets/images/position-on.png',
+      },
+      {
+        pagePath: 'pages/chat/index',
+        text: '聊天',
+        iconPath: 'assets/images/chat.png',
+        selectedIconPath: 'assets/images/chat-on.png',
+      },
+      {
+        pagePath: 'pages/my/index',
+        text: '我的',
+        iconPath: 'assets/images/my.png',
+        selectedIconPath: 'assets/images/my-on.png',
+      },
+    ],
+    color: themeConfig.color,
+    selectedColor: themeConfig.primaryColor,
+    backgroundColor: '#fff',
+    borderStyle: 'black',
+  },
   window: {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#fff',
     navigationBarTitleText: 'WeChat',
     navigationBarTextStyle: 'black'
   }
-})
+});
--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,17 @@
-import { PropsWithChildren } from 'react'
-import { useLaunch } from '@tarojs/taro'
-
-import './app.less'
+import { PropsWithChildren } from 'react';
+import { useLaunch } from '@tarojs/taro';
+import './assets/iconfont/iconfont.css'; // 引入字体图标
+import './app.less';
 
 function App({ children }: PropsWithChildren<any>) {
   useLaunch(() => {
-    console.log('App launched.')
-  })
+    console.log('App launched.');
+  });
 
   // children 是将要会渲染的页面
-  return children
+  return children;
 }
-  
 
 
-export default App
+
+export default App;
