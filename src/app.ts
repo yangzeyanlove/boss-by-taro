70877a5ebe4785652a177e4f822e5620f2305bd2
yangzy01 <yangzy01@mingyuanyun.com>
2024-10-24 14:30:30 +0800

    feat: 样式添加主题色

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import { PropsWithChildren } from 'react';
 import { useLaunch } from '@tarojs/taro';
 import './assets/iconfont/iconfont.css'; // 引入字体图标
-import './app.less';
+import './styles/app.less';
 
 function App({ children }: PropsWithChildren<any>) {
   useLaunch(() => {
