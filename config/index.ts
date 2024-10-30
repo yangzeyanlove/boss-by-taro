4a2b958aa927e8ec696662c3cba27226497c3a65
yangzy01 <yangzy01@mingyuanyun.com>
2024-10-30 10:57:01 +0800

    feat: 开发编译报错修改

--- config/index.ts
+++ config/index.ts
@@ -27,7 +27,14 @@ export default defineConfig<'webpack5'>(async (merge, { command, mode }) => {
       }
     },
     framework: 'react',
-    compiler: 'webpack5',
+    // compiler: 'webpack5',
+    compiler: {   // taro-ui编译出错问题 2024-10-30
+      type: "webpack5",
+      prebundle: {
+        enable: false,
+        force: true,
+      },
+    },
     cache: {
       enable: false // Webpack 持久化缓存配置，建议开启。默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
     },
